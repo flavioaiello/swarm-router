277dbc7ce0499598b169d26be28a829ae510b194
Flavio Aiello <flavio@aiello.ch>
2018-03-20 00:57:52 +0100

    add port config

--- src/listner.go
+++ src/listner.go
@@ -74,6 +74,13 @@ func addBackend(hostname string) {
 	// Add new backend to backend memory map (ttl map pending)
 	log.Printf("Adding %s to swarm-router", hostname)
 	backends[hostname] = 8000
+	/*scanner := bufio.NewWordScanner(os.Getenv("BACKEND_PORTS")))
+	for scanner.Scan() {
+		if strings.HasPrefix(scanner.Text(), hostname) {
+
+			break
+		}
+	}*/
 	// Generate new haproxy configuration
 	executeTemplate("/usr/local/etc/haproxy/haproxy.tmpl", "/usr/local/etc/haproxy/haproxy.cfg")
 	// Restart haproxy using USR2 signal

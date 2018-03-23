81c27a7a04666276d9c83a090ff26a1836a1ca2d
Flavio <flavio@aiello.ch>
2018-03-23 11:34:04 +0100

    Update template.go
    added dnsBackendSuffix to getBackend instead to handle in haproxy template

--- build/src/template.go
+++ build/src/template.go
@@ -2,9 +2,9 @@ package main
 
 import (
   "os"
-	"text/template"
-	"path/filepath"
-	"log"
+  "text/template"
+  "path/filepath"
+  "log"
   "strings"
   "strconv"
 )
@@ -15,32 +15,34 @@ type Config struct {
   Env map[string]string
   HttpBackends map[string]int
   TlsBackends map[string]int
-  DnsBackendSuffix string
 }
 
 func envMap() map[string]string {
-	m := make(map[string]string)
-	for _, kv := range os.Environ() {
-		x := strings.SplitN(kv, "=", 2)
-		m[x[0]] = x[1]
-	}
-	return m
+  m := make(map[string]string)
+  for _, kv := range os.Environ() {
+    x := strings.SplitN(kv, "=", 2)
+    m[x[0]] = x[1]
+  }
+  return m
 }
 
 func getBackend(hostname string) string {
-  if dnsBackendFqdn {
-    return hostname
+  if !dnsBackendFqdn {
+    hostname = strings.Split(hostname, ".")[0]
   }
-  return strings.Split(hostname, ".")[0]
+  if dnsBackendSuffix != "" {
+    hostname = hostname + dnsBackendSuffix
+  }
+  return hostname
 }
 
 func newTemplate(name string) *template.Template {
-	tmpl := template.New(name).Funcs(template.FuncMap{
-		"split": strings.Split,
-		"splitN": strings.SplitN,
+  tmpl := template.New(name).Funcs(template.FuncMap{
+    "split": strings.Split,
+    "splitN": strings.SplitN,
     "getBackend": getBackend,
-	})
-	return tmpl
+  })
+  return tmpl
 }
 
 func executeTemplate(tmpl string, cfg string) {
@@ -50,22 +52,21 @@ func executeTemplate(tmpl string, cfg string) {
   config.Env = envMap()
   config.HttpBackends = httpBackends
   config.TlsBackends = tlsBackends
-  config.DnsBackendSuffix = dnsBackendSuffix
 
   template, err := newTemplate(filepath.Base(tmpl)).ParseFiles(tmpl)
   if err != nil {
-  	log.Fatalf("Unable to parse template: %s", err)
+    log.Fatalf("Unable to parse template: %s", err)
     return
   }
-	file, err := os.Create(cfg)
-	if err != nil {
-		log.Println("create file: ", err)
-		return
-	}
-	err = template.Execute(file, config)
-	if err != nil {
-		log.Print("execute: ", err)
-		return
-	}
-	file.Close()
+  file, err := os.Create(cfg)
+  if err != nil {
+    log.Println("create file: ", err)
+    return
+  }
+  err = template.Execute(file, config)
+  if err != nil {
+    log.Print("execute: ", err)
+     return
+  }
+  file.Close()
 }
